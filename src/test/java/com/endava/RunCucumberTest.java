217f7e53d73e72db76154ec7e0e4f7668ce502bc
francisco R Montufar <jhovanny.rodriguez@globant.com>
2020-03-09 17:52:00 -0500

    Reporting

--- src/test/java/com/endava/RunCucumberTest.java
+++ src/test/java/com/endava/RunCucumberTest.java
@@ -5,6 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+@CucumberOptions(plugin = { "pretty" },
+	format = { "json:cucumber.json" })
 public class RunCucumberTest {
 }
